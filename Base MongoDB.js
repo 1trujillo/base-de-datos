670e3984b05d4a56daa645e2599e7c7077a22b78
Benjamin Polanco <115376642+Stellexd@users.noreply.github.com>
2025-11-27 18:58:58 -0300

    todo listo

--- Base MongoDB.js
+++ Base MongoDB.js
@@ -1,6 +1,14 @@
+b.usuario.insertOne({
+  nombre:"Juan",
+  apellido: "Perez",
+  password: "4321",
+	presupuesto:20000
+});
+
 db.usuario.insertOne({
-  nombre:"benjamin",
-  password: "benjamin",
+  nombre:"Benjamin",
+  apellido: "Polanco",
+  password: "1234",
 	presupuesto:20000
 });
 
@@ -17,12 +25,12 @@ db.subastas.insertMany([{
 },{
   nombre: "Nicol Bolas MTG",
   precio: 10000,
-	tiempo: 0,
-  categoria: ["cartas", "juego de mesa", "coleccionables", "hasbro"],
+	tiempo: 10,
+  categoria: ["cartas", "juego de mesa", "coleccionable", "hasbro"],
 	imagen: "http:Link-Imagen.com",
-	ganador: "Diony",
-	estado: "inactiva",
-	ganada: true,
+	ganador: "Benjamin",
+	estado: "activa",
+	ganada: false,
   fecha: new Date()
 },{
   nombre: "Armadura de Iron Man Mark 1 Original",
@@ -37,29 +45,82 @@ db.subastas.insertMany([{
 },{
 }])
 
+// Simulacion de puja
+
+db.subastas.find({ categoria: "coleccionable" })
+
+db.subastas.updateOne(
+  { nombre: "Nicol Bolas MTG" },
+  { $inc: { precio: 1000 } }
+)
+
+db.subastas.updateOne(
+  { nombre: "Nicol Bolas MTG" },
+  { $set: { ganador : "juan" } }
+)
+
+db.subastas.updateMany(
+  {},
+  { $inc: { tiempo : -10 } }
+)
+
+
+db.subastas.updateOne(
+  { nombre: "Nicol Bolas MTG" },
+  { $set: { ganada : true } }
+)
+
+db.subastas.find({ ganada: true })
+
+db.usuario.updateOne(
+  { nombre: "Juan" },
+  { $inc: { presupuesto : -11000 } }
+)
+
+//fin simulacion
 
 db.ganadores.insertOne([{
   nombreProducto: "Nicol Bolas MTG",
-  usuarioGanador: "Diony",
-  precioFinal:10000,
+  usuarioGanador: "Juan",
+  precioFinal:11000,
   fecha: new Date()
 }])
 
 db.envios.insertOne({
   subastaId: ObjectId('692793dcc5f3a032926f1f4d'),
-  usuario: "Diony",
+  usuario: "Juan",
   nombreProducto: "Nicol Bolas MTG",
   imagen: "https://img.png/",
   precio: 10000,
 
-  direccion: "Calle Falsa 123",
+  direccion: "Calle Portugal 123",
   ciudad: "Santiago",
   region: "RM",
   telefono: "+5691234",
-  nombreCompleto: "Diony Peguero De la Cruz",
+  nombreCompleto: "Juan Perez",
 
   estado: "PENDIENTE_ENVIO",
   fechaCreacion: new Date(),
   fechaEnvio: null,
   fechaEntrega: null
 });
+
+//simulacion 
+
+db.envios.updateOne(
+  {usuario : "Juan"},
+  {$set: {fechaEnvio: new Date}})
+
+db.envios.updateOne(
+  {usuario : Juan},
+  {$set: {fechaEntrega: new Date}})
+
+db.envios.updateOne(
+  {usuario : "Juan"},
+  { $set: { entregado: true } }
+);
+
+
+db.ganadores.deleteOne(
+  {usuarioGanador : "Juan"}
+)
